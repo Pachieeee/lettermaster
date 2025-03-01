1da18229f925cc1d7f1049cb3435a353c87e64a4
pachieeee <sebaspacheco2003@gmail.com>
2025-02-28 23:15:07 -0500

    feat: added principal to routing.

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import {createRouter, createWebHistory} from 'vue-router';
+import PrincipalPageComponent from "../pages/principal.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-
+        { path: '/home', name: 'Home', component: PrincipalPageComponent, meta: { title: 'Home' } },
+        { path: '/', redirect: '/home'}
     ]
 });
 
@@ -13,5 +15,6 @@ router.beforeEach((
     next) => {
     let baseTitle = 'LetterMaster';
     document.title = `${baseTitle} - ${to.meta['title']}`;
+    next();
 });
 export default router;
