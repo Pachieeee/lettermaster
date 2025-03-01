a497fc57916a087bd038d255ec46a102be996b81
pachieeee <sebaspacheco2003@gmail.com>
2025-02-28 23:15:19 -0500

    feat: added routing.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
 import 'primeflex/primeflex.css'
-
+import router from './router/index.js'
 
 const app = createApp(App)
 
@@ -13,5 +13,7 @@ app.use(PrimeVue, {
         preset: Aura
     }
 })
+
+app.use(router)
 
 app.mount('#app')
